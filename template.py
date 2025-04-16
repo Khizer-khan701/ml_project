58fcafca5a02360f53f32526f44e7b803a1a3c16
Khizer-khan701 <khizerdawood987@gmail.com>
2025-04-16 15:59:51 +0500

    Exception Handling done

--- template.py
+++ template.py
@@ -2,15 +2,15 @@ import os
 from pathlib import Path 
 import logging 
 logging.basicConfig(level=logging.INFO)
-project_name="ML Project"
+project_name="mlProject"
 
 list_of_files=[
-    ".github/workflows/.gitkeep",
+    # ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
     f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/model_monitering.py",
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
@@ -21,7 +21,7 @@ list_of_files=[
     "app.py",
     "Dockerfile",
     "requirements.txt",
-    "setup.py"
+    "setup.py",
 
 ]
 for filepath in list_of_files:
@@ -32,7 +32,7 @@ for filepath in list_of_files:
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating Directory: {filedir} for the file {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filename)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
             pass
             logging.info(f"Creating empty file : {filename}")
