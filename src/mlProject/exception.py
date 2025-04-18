bdbced61fdf07e11826742d49032c76de3e31b44
Khizer-khan701 <khizerdawood987@gmail.com>
2025-04-18 15:01:30 +0500

    Data Ingestion

--- src/mlProject/exception.py
+++ src/mlProject/exception.py
@@ -1,11 +1,10 @@
-import sys
 from src.mlProject.logger import logging
+import sys
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
-
+    error_message=f"Error occured in the python script name {file_name} line number {exc_tb.tb_lineno} error message is {str(error)}"
     return error_message
 
 
@@ -16,5 +15,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-        
-
